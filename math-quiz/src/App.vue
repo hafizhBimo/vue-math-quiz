3df6f52d1bf6a2e0d029a82581d4c87d82638632
hafizhBimo <hfzhbimo@gmail.com>
2023-12-02 21:57:26 +0700

    add button component

--- math-quiz/src/App.vue
+++ math-quiz/src/App.vue
@@ -1,47 +1,40 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div id="app">
+    <h1>Math Quiz App</h1>
+    {{ operator }}
+    <OperatorSelector @changeOperator="changeOperator"/>
+  </div>
 </template>
 
+<script>
+import OperatorSelector from './components/OperatorSelector.vue'
+export default {
+    name: "app",
+    data() {
+        return {
+            operator: null
+        };
+    },
+    methods: {
+        changeOperator(operator) {
+            this.operator = operator;
+        }
+    },
+    components: {OperatorSelector }
+}
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
+#app {
+  text-align: center;
+  margin-top: 60px;
+  font-size: 24px;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+button {
+  padding: 10px;
+  width: 160px;
+  cursor: pointer;
 }
 </style>
